e92d477060baa752380ece7d2d364ccdd90a1f6b
esudalaimadan <ezhil.madan@gmail.com>
2020-11-10 17:13:56 +0530

    Scaffold for review block.

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8f5e04b135a93282ce482d1943351aca');
---
+++ plugin.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Plugin Name:     Review Block
+ * Description:     Example block written with ESNext standard and JSX support – build step required.
+ * Version:         0.1.0
+ * Author:          The WordPress Contributors
+ * License:         GPL-2.0-or-later
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:     review-block
+ *
+ * @package         create-block
+ */
+
+/**
+ * Registers all block assets so that they can be enqueued through the block editor
+ * in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/applying-styles-with-stylesheets/
+ */
+function create_block_review_block_block_init() {
+	$dir = dirname( __FILE__ );
+
+	$script_asset_path = "$dir/build/index.asset.php";
+	if ( ! file_exists( $script_asset_path ) ) {
+		throw new Error(
+			'You need to run `npm start` or `npm run build` for the "create-block/review-block" block first.'
+		);
+	}
+	$index_js     = 'build/index.js';
+	$script_asset = require( $script_asset_path );
+	wp_register_script(
+		'create-block-review-block-block-editor',
+		plugins_url( $index_js, __FILE__ ),
+		$script_asset['dependencies'],
+		$script_asset['version']
+	);
+	wp_set_script_translations( 'create-block-review-block-block-editor', 'review-block' );
+
+	$editor_css = 'build/index.css';
+	wp_register_style(
+		'create-block-review-block-block-editor',
+		plugins_url( $editor_css, __FILE__ ),
+		array(),
+		filemtime( "$dir/$editor_css" )
+	);
+
+	$style_css = 'build/style-index.css';
+	wp_register_style(
+		'create-block-review-block-block',
+		plugins_url( $style_css, __FILE__ ),
+		array(),
+		filemtime( "$dir/$style_css" )
+	);
+
+	register_block_type( 'create-block/review-block', array(
+		'editor_script' => 'create-block-review-block-block-editor',
+		'editor_style'  => 'create-block-review-block-block-editor',
+		'style'         => 'create-block-review-block-block',
+	) );
+}
+add_action( 'init', 'create_block_review_block_block_init' );
